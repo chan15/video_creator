1689b2ad0c1dd581c153dbc3986dcba0e88a1d08
David <chan15tw@gmail.com>
2014-03-23 17:00:50 +0800

    add autoPlay preperty

--- js/jquery.video_creator.js
+++ js/jquery.video_creator.js
@@ -3,7 +3,8 @@
         var param = {
             width: 640,
             height: 360,
-            target: ''
+            target: '',
+            autoPlay: false
         }
 
         var set = $.fn.extend(param, options);
@@ -12,20 +13,25 @@
             var url = $(this).prop('href'),
                 id = '',
                 matches = '',
-                result = '';
+                result = '',
+                autoPlay = 0;
+
+            if (true === set.autoPlay) {
+                autoPlay = 1;
+            }
 
             // Youtube
             if (-1 !== url.toLowerCase().indexOf('youtube')) {
                 matches = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
                 id = matches[1];
-                result = '<iframe width="' + set.width + '" height="' + set.height + '" src="//www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>';
+                result = '<iframe width="' + set.width + '" height="' + set.height + '" src="//www.youtube.com/embed/' + id + '?autoplay=' + autoPlay + '" frameborder="0"></iframe>';
             }
 
             // Vimeo
             if (-1 !== url.toLowerCase().indexOf('vimeo')) {
                 matches = url.match(/\/\/(www\.)?vimeo.com\/(\d+)($|\/)/);
                 id = matches[2];
-                result = '<iframe src="http://player.vimeo.com/video/' + id + '?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ffffff" width="' + set.width + '" height="' + set.height + '" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+                result = '<iframe src="http://player.vimeo.com/video/' + id + '?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ffffff&amp;autoplay=' + autoPlay + '" width="' + set.width + '" height="' + set.height + '" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
             }
 
             $(this).on('click', function() {
